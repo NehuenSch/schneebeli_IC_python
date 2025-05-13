56ee3661259aabcb97472f6cfca947da43654393
NehuenSch <schnehuen@gmail.com>
2025-05-13 10:50:58 -0300

    Se sube el primer archivo con tres documentos: Pseudocodigo; Codigo.py; Descripcion. Revisar para una mejor comprension

---
+++ ejercicio_1/Ejercicio_1.psc
@@ -0,0 +1,14 @@
+Algoritmo Ejercicio_1
+	
+	Definir num1, num2, resultado Como Entero
+	
+	Escribir  "Ingrese el primer numero para realizar la suma"
+	Leer num1
+	Escribir  "Ingrese el segundo numero para realizar la suma"
+	Leer num2
+	
+	resultado = num1 + num2
+	
+	Escribir  "El resultado de la sumatoria de sus dos numeros es: ", resultado
+	
+FinAlgoritmo
---
+++ ejercicio_1/Ejercicio_1.py
@@ -0,0 +1,16 @@
+# Este codigo ha sido generado por el modulo psexport 20230904-l64 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+#Se deben pedir dos numero al usuario para realizar la suma de dichos numeros y mostrar por pantalla el resultado
+if __name__ == '__main__': #Se ejecuta todo lo que este abajo de esta
+	num1 = int()
+	num2 = int()
+	resultado = int()
+	print("Ingrese el primer numero para realizar la suma")
+	num1 = int(input())
+	print("Ingrese el segundo numero para realizar la suma")
+	num2 = int(input())
+	resultado = num1+num2
+	print("El resultado de la sumatoria de sus dos numeros es: ",resultado)
+
